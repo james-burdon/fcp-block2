8f6eb1af93142494e04ea952866a43c404baf9b0
james-burton-0 <ky23212@bristol.ac.uk>
2024-02-09 16:57:26 +0000

    test

--- weather.py
+++ weather.py
@@ -2,27 +2,14 @@ import sys
 import csv
 import math
 
-with open('somewhere_better.txt', 'r') as file:
-    reader = csv.reader(file, delimiter=' ')
-    dataold = list(reader)
+def load_an_proc_data(filename):
+    with open(filename, 'r') as file:
+        reader = csv.reader(file, delimiter=' ')
+        dataold = list(reader)
 
+    data = [[val for val in sublist if len(val)>0] for sublist in dataold]
+    numdata = [[float(val) if val.replace('.','').isnumeric() else math.nan for val in sublist] for sublist in data if not any(map(lambda x : x.isalpha(),sublist))]
+    return numdata
 
+numdata = load_an_proc_data('somewhere_better.txt')
 
-# for i in range(dataold):
-#     for index, val in enumerate(dataold[i]):
-#         if val == '':
-#             del(dataold[i][index])
-
-
-
-data = [[val for val in sublist if len(val)>0] for sublist in dataold]
-
-numdata = [[float(val) if val.isnumeric()==True else val for val in sublist] for sublist in data[7:]]
-
-print(numdata)
-
-def annual_average(numdata):
-    for i in range(len(numdata)):
-        temperature,rain=[],[]
-        if numdata[i] not in year and len(year)=0:
-            
