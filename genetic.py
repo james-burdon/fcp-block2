9edbf72761df1029e5ceba88b82bfefa2a51b017
james-burton-0 <ky23212@bristol.ac.uk>
2024-02-13 14:07:00 +0000

    update

--- genetic.py
+++ genetic.py
@@ -3,6 +3,21 @@ import csv
 import math
 
 
-with open(filename, 'r') as file:
-    reader = csv.reader(file, delimiter=' ')
-    dataold = list(reader)
+#filename = sys.argv[1]
+
+def load_an_proc_data(filename):
+    with open(filename, 'r') as file:
+        data = file.read().replace('\n', '')
+    return data
+
+base = {'a': 0, 'c': 0, 't': 0, 'g': 0}
+
+
+def base_count(data,base):
+    for i in range(len(data)):
+        base['{}'.format(data[i])] += 1
+    return base
+
+# for key, value in base.items():
+#     base[key] = value + 1
+base_count(load_an_proc_data('sequence.fasta'),base)
