eed717c555f475d78b7abfc702269b2d6afd7282
james-burton-0 <ky23212@bristol.ac.uk>
2024-02-13 14:54:13 +0000

    final update of wednesday

--- genetic.py
+++ genetic.py
@@ -44,9 +44,9 @@ def complement(seq, reverse=False):
     return complement_seq
 
 def test_rev_comp():
+    assert complement('ATCG')=='TAGC', "complement test"
     assert complement('ATCG', True)=='CGAT', "reverse complement test"
     print("Tests passed")
-    return
 
 print(base_count(load_an_proc_data('sequence.fasta'), base_dict))
 
