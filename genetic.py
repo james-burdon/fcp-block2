d9ad283765c95ec231bab64a9036cdb099944853
james-burdon <eq23636@bristol.ac.uk>
2024-02-15 12:35:29 +0000

    update 2

--- genetic.py
+++ genetic.py
@@ -49,29 +49,31 @@ def test_rev_comp():
     print("Tests passed")
 
 def gc_count(dict):
-    GC=(dict['g']+dict['c'])/(dict['g']+dict['c']+dict['a']+dict['t'])
+    GC=(dict['g']+dict['c'])/(dict['g']+dict['c']+dict['a']+dict['t'])*100
     return GC
 
 def test_gc_content():
     assert gc_count(base_count('ggggaaaaaaaatttatatatcgcc',base_dict))==0.32, "gc_content test"
     print("Tests passed")
 
-def detect_gc_islands(sequence, window_size, gc_threshold):
+def detect_cpg_islands(sequence, window_size, gc_threshold):
     cpg_islands = []
-    #Your code goes here
-    return gc_islands
+    for i in range(len(sequence)-window_size+1):
+        if gc_count(base_count(sequence[i:i+window_size],base_dict))>gc_threshold:
+            cpg_islands.append(sequence[i:i+window_size])
+    return cpg_islands
 
 def main(infile):
-    seq = read_input_file(infile)
+    seq = load_an_proc_data(infile)
     cpg_islands = detect_cpg_islands(seq, window_size=200, gc_threshold=50.0)
 
     print("GC Islands:")
-    for start, end, gc_content in cpg_islands:
-        print(f"Start: {start}, End: {end}, GC Content: {gc_content:.2f}%")
+    for start, end, gc_count in cpg_islands:
+        print(f"Start: {start}, End: {end}, GC Content: {gc_count:.2f}%")
+
+main(filename)
 
 
 
-
-# test_gc_content()
 
 #print(gc_count(base_count(load_an_proc_data(filename),base_dict)))
